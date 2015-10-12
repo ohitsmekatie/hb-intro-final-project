6152ef97c2af4dbf9ab7ad4574fdf6ab82627f8e
Katie Sipos <katie.sipos@gmail.com>
2015-10-11 23:25:53 -0700

    fixing nameType bug and double printing of events

--- next_iteration_game.py
+++ next_iteration_game.py
@@ -1,4 +1,4 @@
-from random import random
+import random
 
 
 class Game(object):
@@ -13,12 +13,13 @@ class Game(object):
             print self.location.description
             self.transition()
 
-            for event in self.location.events:
-                self.money += event.process()
-                if self.money >= 40:
-                    print "You've got enough for pizza - maybe even 2! AW YEAH."
-                    exit(1)
-                print "Pizza Money: %d" % self.money
+            choice = random.choice(self.location.events)
+            self.money += choice.money
+            print choice.message
+            if self.money >= 40:
+                print "You've got enough for pizza - maybe even 2! AW YEAH."
+                exit(1)
+            print "Pizza Money: %d" % self.money
 
     def transition(self):
         transitions = self.location.transitions
@@ -39,20 +40,9 @@ class Place(object):
         self.events = events
 
 class Event(object):
-    def __init__(self, probability, message, money):
-        self.probability = probability
+    def __init__(self, message, money):
         self.message = message
         self.money = money
-
-# TODO something is broken here but i have no idea what!
-# error == self.money += event.process() TypeError: unsupported operand type(s) for +=: 'int' and 'NoneType'
-    def process(self):
-        if random() > self.probability:
-            print self.message
-            return self.money
-        elif random() < self.probability:
-            return 0
-            print "Sorry, there's nothing here!"
 
 class CurrentGame(Game):
     def __init__(self):
@@ -64,20 +54,20 @@ Navigate around the Subway to find $$ to buy pizza
 
 
         lobby = Place("Lobby", "You are in the lobby of the subway",
-            (Event(0.5, "You find a pile of trash on the floor and decide to search it. Woohoo! $5!", 5),
-            Event(0.5, "Notice something shiny in corner. Aw yeah, $1!", 1)))
+            (Event("You find a pile of trash on the floor and decide to search it. Woohoo! $5!", 5),
+            Event("Notice something shiny in corner. Aw yeah, $1!", 1)))
 
         hallway1 = Place("Hallway1", "You are in the upstairs hallway of the subway",
-            (Event(0.5, "A really nice looking dude asks you for money. You give him $1.", -1),
-            Event(0.5, "You search the trashcan for any extra dollaz lying around. Sweet! You find $10!", 10)))
+            (Event("A really nice looking dude asks you for money. You give him $1.", -1),
+            Event("You search the trashcan for any extra dollaz lying around. Sweet! You find $10!", 10)))
 
         upstairs = Place("Upstairs Platform", "You are on the upstairs platform of the subway",
-            (Event(.5, "You see a backpack in the corner. $5!", 5),
-            Event(.5, "There's a guy giving out free money!! Iknorite? $3",3)))
+            (Event("You see a backpack in the corner. $5!", 5),
+            Event("There's a guy giving out free money!! Iknorite? $3",3)))
 
         subwayCar = Place("Subway Car", "You are in the car of the subway ",
-            (Event(.5, "You see a fanny pack lying on the ground. Awesome, $20", 5),
-            Event(.5, "Some older nice lookin' lady asks you for money. You give her $5", -5)))
+            (Event("You see a fanny pack lying on the ground. Awesome, $5", 5),
+            Event("Some older nice lookin' lady asks you for money. You give her $5", -5)))
 
 
         lobby.transitions = (hallway1,)
