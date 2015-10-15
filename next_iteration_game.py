657181794a9023744cfd87bad5ab7ee404f0ba85
Katie Sipos <katie.sipos@gmail.com>
2015-10-15 14:37:14 -0700

    adding new events to rooms

--- next_iteration_game.py
+++ next_iteration_game.py
@@ -1,9 +1,33 @@
 import random
 
+# TODO add pizza method  store at the end of game
+# TODO you need to fix what happens when people press a # that's not 1 or 2!
+# TODO add some more events to maybe make the game more interesting?
+# TODO style that shit, yo!
+# TODO do you want to try to add probability?
+# TODO what about adding some bad guys?
+
+# def pizza_store():
+#     print """
+#     You have magically been transformed to Spak Brothers Pizza in Pittsburgh, PA.
+#     It's seriously your lucky day. TRUST ME.
+#
+#     MENU:
+#     1. Pizza, $10
+#     2. Pizza with topping, $15
+#     3. Seitan BBQ sub, $10
+#     4. Coke, $2
+#     5. Fries, $5
+#     6. Fries with cheese, $9
+#     7. Salad, $10
+#     """
+#     response = raw_input("What would you like get? You have %i to spend") % (self.money)
+
+
 
 class Game(object):
     def __init__(self):
-        self.money = 0
+        self.money = 1
 
     def play(self):
         print self.introduction
@@ -17,7 +41,7 @@ class Game(object):
             self.money += choice.money
             print choice.message
             if self.money >= 40:
-                print "You've got enough for pizza - maybe even 2! AW YEAH."
+                print "\n\nYou've have $%i! You definitely have enough for a pizza - MAYBE EVEN 2! " % (self.money)
                 exit(1)
             print "Pizza Money: %d" % self.money
 
@@ -55,19 +79,23 @@ Navigate around the Subway to find $$ to buy pizza
 
         lobby = Place("Lobby", "You are in the lobby of the subway",
             (Event("You find a pile of trash on the floor and decide to search it. Woohoo! $5!", 5),
-            Event("Notice something shiny in corner. Aw yeah, $1!", 1)))
+            Event("You notice something shiny in corner. Aw yeah, $1!", 1),
+            Event("$10 magically drops into your hand from the ceiling!", 10)))
 
         hallway1 = Place("Hallway1", "You are in the upstairs hallway of the subway",
             (Event("A really nice looking dude asks you for money. You give him $1.", -1),
-            Event("You search the trashcan for any extra dollaz lying around. Sweet! You find $10!", 10)))
+            Event("You search the trashcan for any extra dollaz lying around. Sweet! You find $10!", 10),
+            Event("You see a book lying on the floor. Inside of it is $3! YAY!", 3)))
 
         upstairs = Place("Upstairs Platform", "You are on the upstairs platform of the subway",
             (Event("You see a backpack in the corner. $5!", 5),
-            Event("There's a guy giving out free money!! Iknorite? $3",3)))
+            Event("There's a guy giving out free money!! Iknorite? $3",3),
+            Event("You got robbed. Sad panda", -30)))
 
         subwayCar = Place("Subway Car", "You are in the car of the subway ",
-            (Event("You see a fanny pack lying on the ground. Awesome, $5", 5),
-            Event("Some older nice lookin' lady asks you for money. You give her $5", -5)))
+            (Event("You see a fanny pack lying on the ground. Awesome, $30", 30),
+            Event("Some older nice lookin' lady asks you for money. You give her $5", -5),
+            Event("You look around the floor and find...nothing. Sorry bout it!",0)))
 
 
         lobby.transitions = (hallway1,)
